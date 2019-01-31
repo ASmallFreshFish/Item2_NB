19b190f62bd6cf90aedb92459ea1d2413735d465
zhoujianwen <jianwen.zhou@altenchina.com>
2019-01-31 11:07:44 +0800

    检测按键，并通过NB_IOT平台发送
    信息：
    1、20ms中断扫描按键，主流城更新信息；
    2、通过NB发送。
    
    待优化：
    1、按键扫描数量；
    2、增加NB发送失败的重发机制。

---
+++ NB_IOT_Key/Project/Test/MDK-ARM/STM32L152-EVAL/dht11.d
@@ -0,0 +1,1 @@
+.\stm32l152-eval\dht11.o: ..\..\HARDWARE\DHT11\dht11.c
---
+++ NB_IOT_Key/Project/Test/main.c
@@ -0,0 +1,107 @@
+/*
+  ******************************************************************************
+  * @file    Project/STM32L1xx_StdPeriph_Templates/main.c 
+  * @author  MCD Application Team
+  * @version V1.2.0
+  * @date    16-May-2014
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+*/
+
+/*
+	利用COAP协议南向开发，将数据发送到电信平台并进行显示。这里术语称为南向开发
+	注意电信的卡只能接入电信的IOT平台，对应移动的可以任意接入电信或者是华为的平台。
+	如果需要开发oceanconncet的话，需要采用移动的B8系列开发。
+*/
+
+#include "head_include.h"
+
+//u8 temp,humi;
+//u8 sendata[7]="001234";	
+u8 sendata[SEND_DATA_LEN]={'0'};
+u8 send_buf[SEND_BUF_LEN]={'0'};
+u8 upload_buf_sequence = 1;
+u8 volatile write_location = 0;
+u8 volatile read_location = 0;
+u8 read_data_success_flag = 0;
+u8 write_data_success_flag = 0;
+u8 upload_main_loop_count = 0;
+
+#ifdef DEBUG_MACRO
+extern u8 debug_char;
+#endif
+
+int main(void)
+{
+	main_init();
+	
+	while (1)
+	{	
+		upload_buf_write();
+		upload_handle();
+		LED_handle();
+	}
+}
+
+void main_init(void)
+{
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); // interrupt priority level set
+    delay_init();	
+    LED_Init();
+	KEY_init();
+//	ID 温度 湿度
+//  while(DHT11_Init());//初始化DHT11
+    uart_init(9600);          
+    uart3_init(9600);
+	TIM3_Int_Init(199,3199);   // 20ms一次中断
+//	TIM3_Int_Init(299,3199);  // 30ms一次中断
+    TIM4_Int_Init(4999,3199); // 500ms一次中断
+    CDP_Init();//CDP服务器初始化    
+    BC95_Init();
+	upload_init();
+	
+	KEY_scan_start();
+}
+
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+
+
+
